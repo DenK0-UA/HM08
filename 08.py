d4993f5ec14bc04c53ac31195c7fdfabd5b99b09
DenK0 <denidinamo@gmail.com>
2023-12-06 18:11:35 +0200

    8.01

--- 08.py
+++ 08.py
@@ -1,24 +1,21 @@
-from decimal import Decimal, getcontext
+from collections import deque
 
-def decimal_average(number_list, signs_count):
-    getcontext().prec = signs_count  # Встановлюємо точність для Decimal
+MAX_LEN = 10
 
-    sum_decimal = Decimal(0)  # Ініціалізуємо суму змінних типу Decimal
-    count = len(number_list)  # Кількість чисел у списку
-
-    for num in number_list:
-        sum_decimal += Decimal(str(num))  # Додаємо число до суми
-
-    if count > 0:
-        average_decimal = sum_decimal / count  # Обчислюємо середнє арифметичне
-        return average_decimal
-
-    return Decimal(0)  # Повертаємо 0, якщо список чисел порожній
+fifo = deque(maxlen=MAX_LEN)
 
 
-# Приклади використання
-result1 = decimal_average([3, 5, 77, 23, 0.57], 4)
-print(result1)  # Вивід: 21.714
+def push(element):
+    fifo.append(element)
 
-result2 = decimal_average([31, 55, 177, 2300, 1.57], 5)
-print(result2)  # Вивід: 512.91400
+def pop():
+    return fifo.pop()
+
+
+push(3)
+push(10)
+push(9)
+
+print(pop())
+print(pop())
+print(pop())
--- 08_.py
+++ 08_.py
@@ -1,28 +1,21 @@
-import collections
+from collections import deque
 
-Cat = collections.namedtuple("Cat", ["nickname", "age", "owner"])
+MAX_LEN = 10
 
-def convert_list(cats):
-    if isinstance(cats[0], dict):  # Перевіряємо, чи cats - список словників
-        converted_list = []
-        for cat_dict in cats:
-            cat_namedtuple = Cat(**cat_dict)  # Створюємо іменований кортеж зі словника
-            converted_list.append(cat_namedtuple)
-        return converted_list
-    else:
-        converted_list = []
-        for cat_namedtuple in cats:
-            cat_dict = cat_namedtuple._asdict()  # Перетворюємо іменований кортеж у словник
-            converted_list.append(cat_dict)
-        return converted_list
+lifo = deque(maxlen=MAX_LEN)
+
+def push(element):
+    lifo.appendleft(element)
+
+def pop():
+    return lifo.popleft()
 
 
-# Приклад використання
-cats_list = [
-    Cat("Mick", 5, "Sara"),
-    Cat("Barsik", 7, "Olga"),
-    Cat("Simon", 3, "Yura")
-]
 
-converted_dict_list = convert_list(cats_list)
-print(converted_dict_list)
+push(3)
+push(10)
+push(9)
+
+print(pop())  # Виведе: 3
+print(pop())  # Виведе: 2
+print(pop())  # Виведе: 1
