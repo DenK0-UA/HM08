df03be2f809d590dbadee1e0a9b3e9027f403a06
DenK0 <denidinamo@gmail.com>
2023-12-08 00:26:42 +0200

    done///

--- main.py
+++ main.py
@@ -1,14 +1,48 @@
-from datetime import date, datetime
+from datetime import date, datetime, timedelta
 
 
 def get_birthdays_per_week(users):
-    # Реалізуйте тут домашнє завдання
-    return users
+    if not users:
+        return {}
+
+    today = date.today()
+    current_day = {today.strftime('%A'): today}
+    for i in range(1, 7):
+        today += timedelta(days=1)
+        current_day[today.strftime('%A')] = today
+
+    result = {}
+
+    for user in users:
+        current_users = user["birthday"]
+
+        for weekday in current_day:
+            current_today = current_day[weekday]
+
+            if not (current_today.month == current_users.month and current_today.day == current_users.day):
+                continue
+
+            if weekday in ('Sunday', 'Saturday'):
+                if 'Monday' in result:
+                    result['Monday'].append(user["name"])
+                else:
+                    result['Monday'] = [user["name"]]
+
+            else:
+                if weekday in result:
+                    result[weekday].append(user["name"])
+                else:
+                    result[weekday] = [user["name"]]
+
+    return result
 
 
 if __name__ == "__main__":
     users = [
-        {"name": "Jan Koum", "birthday": datetime(1976, 1, 1).date()},
+        {"name": "Jan", "birthday": datetime(1976, 11, 24).date()},
+        {"name": " Koum", "birthday": datetime(1976, 11, 22).date()},
+        {"name": "JK", "birthday": datetime(1976, 11, 25).date()},
+        {"name": "Heho", "birthday": datetime(1976, 11, 26).date()},
     ]
 
     result = get_birthdays_per_week(users)
